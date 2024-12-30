a64e530e5f07ef903652279b03dc8033fc4a797a
datadiego <juandiegomariscal@gmail.com>
2024-12-30 22:39:59 +0100

    update

--- utils/get_dicts.sh
+++ utils/get_dicts.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 wget -O utils/dict.txt https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
-
-exit 0
